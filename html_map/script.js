6139eb5b4278f6bac231c0763b0370ab0b3838ef
juankarlos999 <juan_karlos_999@hotmail.com>
2020-10-17 11:31:56 -0500

    add new funtions

--- html_map/script.js
+++ html_map/script.js
@@ -1,7 +1,12 @@
 var map, geometryOutputOrigin, geometryOutputDest, inputOrigin, inputDest;
 const image =
 "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
-
+if(sessionStorage.getItem('lat')){
+  console.log('storage de inicio')
+  sessionStorage.clear();
+} else {
+  console.log('Sin storage de inicio')
+}
 // Create Map location Colombia
 function createMap(){
   var options = {
@@ -48,20 +53,43 @@ function createMap(){
   );
   }  
 
-    // Request Api for Array of markers
-    var urlServ = 'http://quickapi-env.eba-tkndff3x.us-east-1.elasticbeanstalk.com/api/tolls';
-    var requestTolls = new Request(urlServ, {
-      method: 'GET',
-      mode: 'cors'
+    // Request API Rest for Array of tolls markers or Routes and Cost
+    var requestData = {"points": [
+      {
+        "lat": 10.3932277, 
+        "lng": -75.4832311
+
+      },
+      {
+        "lat": 4.710988599999999, 
+        "lng": -74.072092
+      }
+    ],
+    "vehicle": {
+        "name": "Camioneta"
+    }
+    }
+    // 4.7109851,-74.1421321
+   // 8.842959 -76.438679
+    var urlServTolls = 'http://quickapi-env.eba-tkndff3x.us-east-1.elasticbeanstalk.com/api/tolls';
+    var urlServApi = 'http://quickapi-env.eba-tkndff3x.us-east-1.elasticbeanstalk.com/api/routes/tolls';
+
+    var requestTolls = new Request(urlServApi, {
+      method : 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body : JSON.stringify(requestData)
      });
     async function apiTolls(){
       const response = await fetch(requestTolls);
       const respTolls = await response.json();
-      console.log(respTolls);
+      console.log(respTolls.tolls);
       // Loop through data(coordinates - name)
-      for(i of respTolls.data.tolls){
+      for(i of respTolls.tolls){
         addMarker(i);
-        //console.log(i.name);
+       console.log(i.coordinates);
       }
       // Add Marker Function 
       function addMarker(props){
@@ -98,14 +126,16 @@ function geocodeOrigin(e){
   // Prevent actual submit
   e.preventDefault();
   var locationOrign = document.getElementById('location-input-1').value;
+  
   if(!locationOrign){
     alert("Ups! your ORIGIN is EMPTY. Please try again.");
-  } else console.log(locationOrign);
-
+  } else {
+    console.log(locationOrign);
+    }
   axios.get('https://maps.googleapis.com/maps/api/geocode/json',{
     params:{
       address: locationOrign,
-      key:'KEY-APIGOOGLE'
+      key:'AIzaSyDUU1bnQ03PRR_wBR6uCV9hqXDMk4TlQG0'
     }
   })
   .then(function(response){
@@ -114,8 +144,10 @@ function geocodeOrigin(e){
     var lat = response.data.results[0].geometry.location.lat;
     var lng = response.data.results[0].geometry.location.lng;
     geometryOutputOrigin = {lat:lat, lng:lng};
-    console.log(geometryOutputOrigin);
-
+    console.log('Origin:', geometryOutputOrigin);
+    sessionStorage.setItem('lat', lat);
+    sessionStorage.setItem('lng', lng);
+    
     // Test coordenates
     //document.getElementById('geometryO').innerHTML = `Location Origin <br>LAT: ${geometryOutputOrigin.lat} <br>LNG: ${geometryOutputOrigin.lng}`;
   })
@@ -140,7 +172,7 @@ function geocodeDest(e){
   axios.get('https://maps.googleapis.com/maps/api/geocode/json',{
     params:{
       address: locationDest,
-      key:'KEY-APIGOOGLE'
+      key:'AIzaSyDUU1bnQ03PRR_wBR6uCV9hqXDMk4TlQG0'
     }
   })
   .then(function(response){
@@ -149,7 +181,7 @@ function geocodeDest(e){
     var lat = response.data.results[0].geometry.location.lat;
     var lng = response.data.results[0].geometry.location.lng;
     geometryOutputDest = {lat:lat, lng:lng};
-    console.log(geometryOutputDest);
+    console.log('Destination:', geometryOutputDest);
 
     // Test coordenates
     //document.getElementById('geometryD').innerHTML = `Location Destination <br>LAT: ${geometryOutputDest.lat} <br>LNG: ${geometryOutputDest.lng}`;
@@ -164,10 +196,52 @@ function geocodeDest(e){
 }
 
 // Catch inputs type fuel and vehicle
-function typeVehFuel(){
+async function typeVehFuel(){
   var typeVehicle = document.getElementById('vehicle');
   var optionVehicle= typeVehicle.options[typeVehicle.selectedIndex].value;
   var typeFuel = document.getElementById('fuel');
   var optionFuel= typeFuel.options[typeFuel.selectedIndex].value;
   console.log(optionFuel, optionVehicle);
+  // Return la información en sessionStorage
+  console.log('Test var lat Origen:', sessionStorage.getItem('lat'));
+  console.log('Test var lng Origin:', sessionStorage.getItem('lng'));
 }
+/*
+// method POST Api Route-Tolls
+var urlaux = 'http://quickapi-env.eba-tkndff3x.us-east-1.elasticbeanstalk.com/api/routes/tolls';
+var requestData = {"points": [
+  {
+    "lat": 8.842959, 
+    "lng": -76.438679
+  },
+  {
+    "lat": 6.413056, 
+    "lng": -75.705379
+  }
+],
+"vehicle": {
+    "name": "Camioneta"
+}
+}
+  
+  const usersPromise = fetch(urlaux, {
+    method : 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body : JSON.stringify(requestData)
+  }).then(response => {
+    if (!response.ok) {
+      throw new Error("Error response from API server.");
+    }
+    return response.json();
+  }).then(responseData => {
+    return responseData;
+  });
+  
+  usersPromise.then(dataResp => {
+    console.log("response apiRouteTolls: ", dataResp);
+  }, error => {
+    console.error("Failed to fetch route and tolls due to error: ", error);
+  }); */
