15e77246afc0defaa5d0762fe184e551cff2d559
andres reyes <51345583+andresvanegas19@users.noreply.github.com>
2020-10-20 22:54:44 -0500

    Delete vehiculos.js

--- routes/vehiculos.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controllersVehiculo = require('../controllers/vehiculos')
-router.get('/', controllersVehiculo.getVehiculos);
-router.post('/', controllersVehiculo.createVehiculos);
-module.exports = router;
-
